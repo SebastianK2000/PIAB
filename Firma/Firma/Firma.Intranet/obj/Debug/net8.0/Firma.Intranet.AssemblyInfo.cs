5db89813be5b55ea93b62a0a2ed3cc4881195142
Sebastian <sebastian.kosciolek2000@gmail.com>
2025-05-03 17:43:59 +0200

    feat: update and refactor intranet

--- Firma/Firma/Firma.Intranet/obj/Debug/net8.0/Firma.Intranet.AssemblyInfo.cs
+++ Firma/Firma/Firma.Intranet/obj/Debug/net8.0/Firma.Intranet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Firma.Intranet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2ded2979f0c94148f5853212d671bf67993ea05")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50233d30524864450fe197b689a6d773785dc988")]
 [assembly: System.Reflection.AssemblyProductAttribute("Firma.Intranet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Firma.Intranet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
